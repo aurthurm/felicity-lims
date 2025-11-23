1e64f524b80603a77a6ecdb2032361e82e78e4f6
Aurthur Musendame <aurthurmusendame@gmail.com>
2025-11-23 22:27:40 +0200

    upgrading tw to v4

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const { purge } = require('tailwindcss/stubs/config.full.js');
-
-const autoprefixer = require('autoprefixer')();
-const tailwindcss = require('tailwindcss')('./tailwind.config.js');
-
-module.exports = {
-    plugins: [tailwindcss, autoprefixer],
-    ...(process.env.NODE_ENV === 'production' ? [purge] : []),
-};
--- vite.config.js
+++ vite.config.js
@@ -3,36 +3,20 @@ import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import path from 'path';
-// import { fileURLToPath, URL } from 'url';
+import tailwindcss from "@tailwindcss/vite";
 
 /**
  * https://vitejs.dev/config/
  * @type {import('vite').UserConfig}
  */
 export default defineConfig({
-    plugins: [tsConfigPaths(), vue(), vueJsx()],
+    plugins: [tsConfigPaths(), vue(), vueJsx(), tailwindcss()],
     build: {
         target: 'esnext',
-        // rollupOptions: {
-        //     output:{
-        //         manualChunks(id) {
-        //             if (id.includes('node_modules')) {
-        //                 return id.toString().split('node_modules/')[1].split('/')[0].toString();
-        //             }
-        //         }
-        //     }
-        // }
     },
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './webapp'),
-            // '@': fileURLToPath(new URL('./webapp', import.meta.url))
-            // "@": path.resolve(path.dirname(fileURLToPath(import.meta.url)), "webapp"),
-        },
-    },
-    css: {
-        postcss: {
-            plugins: [require('tailwindcss'), require('autoprefixer')],
         },
     },
 });
